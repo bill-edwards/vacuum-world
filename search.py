3b4e49b3fd639cfda9d4dc15d39905400e2f2a12
Bill Edwards <winner.bill@gmail.com>
2018-05-30 13:21:28 +0100

    Implement uniform-cost graph-search

--- search.py
+++ search.py
@@ -48,8 +48,48 @@ class FIFOFrontier(object):
 	def size(self):
 		return len(self.set)
 
+class PriorityQueueFrontier(object):
 
-# The search algorithms themselves	
+	def __init__(self):
+		self.queue = []
+		self.set = set()
+
+	def add(self, node):
+		self.queue.append(node)
+		self.queue.sort(key = lambda node: node.path_cost, reverse=True)
+		self.set.add(node.state.stringify())
+
+	def pop(self):
+		node = self.queue.pop()
+		self.set.discard(node.state.stringify())
+		return node
+
+	def test_membership(self, state):
+		return state.stringify() in self.set
+
+	def size(self):
+		return len(self.set)
+
+	def get_path_cost_to_state(self, state):
+		print 'GPC'
+		nodes = [node for node in self.queue if node.state.stringify() == state.stringify()]
+		if len(nodes) == 0: return None
+		else:
+			return nodes[0].path_cost
+
+	# Given a state S and a node N with matching state, replaces any node N' whose state is S with N.
+	def replace_node(self, state, node):
+		print 'RN'
+		indices = [index for (index, node) in enumerate(self.queue) if node.state.stringify() == state.stringify()]
+		if len(indices) != 0:
+			index = indices[0]
+			self.queue[index] = node
+			self.queue.sort(key = lambda node: node.path_cost, reverse=True)
+
+
+# The search algorithms themselves
+
+# TO DO: (i) Record actual time taken to find solution; (ii) Record number of nodes expanded; (iii) Make printing to screen optional.
 
 def breadth_first_graph_search(problem, initial_state):
 
@@ -72,3 +112,27 @@ def breadth_first_graph_search(problem, initial_state):
 			if (child_node.state.stringify() not in explored and not frontier.test_membership(child_node.state)):
 				if problem.goal_test(child_node.state): return generate_solution(child_node)
 				frontier.add(child_node)
+
+def uniform_cost_graph_search(problem, initial_state):
+
+	if problem.goal_test(initial_state): return []
+
+	node = Node(initial_state, None, None, 0)
+	frontier = PriorityQueueFrontier()
+	frontier.add(node)
+	explored = set()
+	i = 0
+
+	while True:
+		print i, frontier.size()
+		i += 1
+		if frontier.size() == 0: return 'FAILURE'
+		node = frontier.pop()
+		if problem.goal_test(node.state): return generate_solution(node)
+		explored.add(node.state.stringify())
+		for action in problem.actions(node.state):
+			child_node = generate_child_node(problem, node, action)
+			if (child_node.state.stringify() not in explored and not frontier.test_membership(child_node.state)):
+				frontier.add(child_node)
+			elif (frontier.test_membership(child_node.state) and frontier.get_path_cost_to_state(child_node.state) > child_node.path_cost):
+				frontier.replace_node(child_node.state, child_node)
