5261bdc48a1700b4d4764de28a66af5a6f2bcfd2
Bill Edwards <winner.bill@gmail.com>
2018-07-20 17:20:45 +0100

    Little naming change

--- agent/te_specific/problem.py
+++ agent/te_specific/problem.py
@@ -1,20 +1,20 @@
 class Problem(object):
 
 	def __init__(self, task_env):
-		self.floor = task_env.environment
+		self.env = task_env.environment
 		self.pm = task_env.performance_measure
 
 	def actions(self, state):
-		return [law['action'] for law in self.floor.laws]
+		return [law['action'] for law in self.env.laws]
 
 	# Returns the outcome State. Used for planning in deterministic task environments.
 	def result(self, state, action):
-		return self.floor.tick(state, action)
+		return self.env.tick(state, action)
 
 	# Returns an array of possible outcome States. Used for planning in non-deterministic task environments.
 	# TO DO: Right now can be duplications if multiple outcomes yield the same state e.g. when succeeding in moving left into a wall, or slipping and failing to move. 
 	def results(self, state, action):
-		law = [law for law in self.floor.laws if law['action'] == action][0]
+		law = [law for law in self.env.laws if law['action'] == action][0]
 		if 'state_transform' in law:
 			return [law['state_transform'](state)]
 		elif 'state_transforms' in law:
